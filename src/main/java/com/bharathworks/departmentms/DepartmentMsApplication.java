636f45fdc7eb84e1406606a5e1c0b0ae6d3c4783
bharathkh123 <70330347+bharathkh123@users.noreply.github.com>
2021-05-22 16:26:51 +0530

    eureka setup

--- src/main/java/com/bharathworks/departmentms/DepartmentMsApplication.java
+++ src/main/java/com/bharathworks/departmentms/DepartmentMsApplication.java
@@ -2,8 +2,10 @@ package com.bharathworks.departmentms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DepartmentMsApplication {
 
 	public static void main(String[] args) {
